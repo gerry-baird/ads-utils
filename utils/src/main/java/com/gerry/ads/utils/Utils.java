e3c12afa448c2d21ad5adb30dfa34862137cd35f
gerry <gerry.baird@uk.ibm.com>
2021-10-05 11:52:58 +0100

    Trying build without local install

--- utils/src/main/java/com/gerry/ads/utils/Utils.java
+++ utils/src/main/java/com/gerry/ads/utils/Utils.java
@@ -3,16 +3,18 @@ package com.gerry.ads.utils;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import com.ibm.rules.engine.annotations.PureFunction;
 
 public class Utils {
 
+    @PureFunction
     public static int getAge(Date now, Date dateOfBirth) {
         LocalDate currentDate = convertToLocalDateViaSqlDate(now);
         LocalDate birthDate = convertToLocalDateViaSqlDate(dateOfBirth);
 
         if ((birthDate != null) && (currentDate != null)) {
             //return Period.between(birthDate, currentDate).getYears();
-            long years = ChronoUnit.YEARS.between(currentDate, birthDate);
+            long years = ChronoUnit.YEARS.between(birthDate, currentDate);
             return (int)years;
         } else {
             return 0;
--- utils/src/test/java/com/gerry/ads/utils/UtilsTest.java
+++ utils/src/test/java/com/gerry/ads/utils/UtilsTest.java
@@ -14,8 +14,8 @@ class UtilsTest {
         String sNow="28/06/2021";
         String sDob="15/06/1971";
 
-        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sNow);
-        Date now = new SimpleDateFormat("dd/MM/yyyy").parse(sDob);
+        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sDob);
+        Date now = new SimpleDateFormat("dd/MM/yyyy").parse(sNow);
 
         int age = Utils.getAge(now, dob);
 
@@ -28,12 +28,26 @@ class UtilsTest {
         String sNow="01/05/2021";
         String sDob="15/06/2015";
 
-        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sNow);
-        Date now = new SimpleDateFormat("dd/MM/yyyy").parse(sDob);
+        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sDob);
+        Date now = new SimpleDateFormat("dd/MM/yyyy").parse(sNow);
 
         int age = Utils.getAge(now, dob);
 
         assertEquals(5, age);
+    }
+
+    @Test
+    void testGetAge16() throws ParseException {
+
+        String sNow="04/10/2021";
+        String sDob="26/09/2005";
+
+        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sDob);
+        Date now = new SimpleDateFormat("dd/MM/yyyy").parse(sNow);
+
+        int age = Utils.getAge(now, dob);
+
+        assertEquals(16, age);
     }
 
 }
